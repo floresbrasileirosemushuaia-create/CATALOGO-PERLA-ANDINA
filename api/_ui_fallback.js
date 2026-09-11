72f255d2fe7be1f8c190f01f990bc3a474241641
floresbrasileirosemushuaia-create <floresbrasileirosemushuaia@gmail.com>
2026-09-11 15:29:59 -0300

    V2.5.1 lote A - fallback UI final

--- api/_ui_fallback.js
+++ api/_ui_fallback.js
@@ -8,19 +8,9 @@ const DATA=[
   require('./_ui_b_04'),
   require('./_ui_c_00'),
   require('./_ui_c_01'),
-  require('./_ui_c_02'),
-  require('./_ui_c_03'),
-  require('./_ui_c_04'),
-  require('./_ui_c_05'),
-  require('./_ui_c_06'),
-  require('./_ui_c_07'),
-  require('./_ui_c_08'),
-  require('./_ui_c_09'),
-  require('./_ui_c_10'),
-  require('./_ui_c_11'),
-  require('./_ui_c_12'),
-  require('./_ui_c_13'),
-  require('./_ui_c_14'),
+  require('./_ui_d_00'),
+  require('./_ui_d_01'),
+  require('./_ui_d_02'),
 ];
 let cache='';
 function fallbackUi(){if(!cache)cache=zlib.brotliDecompressSync(Buffer.from(DATA.join(''),'base64')).toString('utf8');return cache}
