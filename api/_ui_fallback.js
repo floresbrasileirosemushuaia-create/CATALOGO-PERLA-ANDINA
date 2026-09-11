a783da83ca8aec91817c45657761ca02a07cb107
floresbrasileirosemushuaia-create <floresbrasileirosemushuaia@gmail.com>
2026-09-11 15:22:06 -0300

    V2.5.1 lote A - fallback UI Brotli

--- api/_ui_fallback.js
+++ api/_ui_fallback.js
@@ -1,13 +1,32 @@
 'use strict';
 const zlib=require('zlib');
-const c0=require('./_ui_fb_00');
-const c1=require('./_ui_fb_01');
-const c2=require('./_ui_fb_02');
-const c3=require('./_ui_fb_03');
-const c4=require('./_ui_fb_04');
-const c5=require('./_ui_fb_05');
-const c6=require('./_ui_fb_06');
-const c7=require('./_ui_fb_07');
-const DATA=c0+c1+c2+c3+c4+c5+c6+c7;
-function fallbackUi(){return zlib.gunzipSync(Buffer.from(DATA,'base64')).toString('utf8')}
+const DATA=[
+  require('./_ui_b_00'),
+  require('./_ui_b_01'),
+  require('./_ui_b_02'),
+  require('./_ui_b_03'),
+  require('./_ui_b_04'),
+  require('./_ui_b_05'),
+  require('./_ui_b_06'),
+  require('./_ui_b_07'),
+  require('./_ui_b_08'),
+  require('./_ui_b_09'),
+  require('./_ui_b_10'),
+  require('./_ui_b_11'),
+  require('./_ui_b_12'),
+  require('./_ui_b_13'),
+  require('./_ui_b_14'),
+  require('./_ui_b_15'),
+  require('./_ui_b_16'),
+  require('./_ui_b_17'),
+  require('./_ui_b_18'),
+  require('./_ui_b_19'),
+  require('./_ui_b_20'),
+  require('./_ui_b_21'),
+  require('./_ui_b_22'),
+  require('./_ui_b_23'),
+  require('./_ui_b_24'),
+];
+let cache='';
+function fallbackUi(){if(!cache)cache=zlib.brotliDecompressSync(Buffer.from(DATA.join(''),'base64')).toString('utf8');return cache}
 module.exports={fallbackUi};
